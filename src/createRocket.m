2899f0b7b9030ca1d654ddbba7393e6a314cc74f
ExpiredGrammar <eakers1123@gmail.com>
2016-03-31 11:02:50 -0400

    Delete createRocket.m

--- src/createRocket.m
+++
@@ -1,9 +0,0 @@
-function [ output_args ] = createRocket( fuel, engine, payload )
-%createRocket Summary of this function goes here
-%   Detailed explanation goes here
-
-% height = calculate with fuel
-% 
-
-end
-
