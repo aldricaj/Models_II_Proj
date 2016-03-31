f625b39e48aca1cd15a25f7a0ae1cc68c8086e55
ExpiredGrammar <eakers1123@gmail.com>
2016-03-31 11:02:28 -0400

    Added files via upload

---
+++ src/CreateRocket.m
@@ -0,0 +1,9 @@
+function [ output_args ] = CreateRocket( fuel, engine, payload )
+%createRocket Summary of this function goes here
+%   Detailed explanation goes here
+
+% height = calculate with fuel
+% 
+
+end
+
---
+++ src/LaunchRocket.m
@@ -0,0 +1,7 @@
+function [ output_args ] = LaunchRocket( rocket )
+%LaunchRocket Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
