59933429356aef55e71e715ae58656145a160fb1
Andrew Aldrich <Andrew1Aldrich@hotmail.com>
2016-04-05 08:42:59 -0400

    Stub function for runSim

---
+++ src/runSim.m
@@ -0,0 +1,6 @@
+function [outStruct] = runSim(launchAngle, thrust,payloadMass, planetRadius, planetMass, height,verbose=0)
+% outStruct = runSim(launchAngle, thrust,payloadMass, planetRadius, planetMass, height,verbose=0)
+% 
+% outStruct = runSim(launchAngle, thrust,payloadMass, planetRadius, planetMass, height)
+
+end
